a81ddf8973bccf8cb08c97214cfc88fb025c735d
Aswin A V <aswinavofficial@gmail.com>
2020-05-01 03:20:00 +0530

    LeetCode 125. Valid Palindrome Solved

--- strings/LC125_isPalindrome/main.py
+++ strings/LC125_isPalindrome/main.py
@@ -1,2 +1,19 @@
-class Solution:
+class Solution(object):
     def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        import re
+        
+        s = re.sub(r'[\W_]','',s).lower()
+        
+        left = 0
+        right = len(s) - 1
+        
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left = left +1
+            right = right -1
+        return True
