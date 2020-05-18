9a32fae6510a66ea552a070e527d762c2464261b
Aswin A V <aswinavofficial@gmail.com>
2020-05-19 01:05:59 +0530

    Solved LC242 - Anagram Check

--- strings/LC242_isAnagram/main.py
+++ strings/LC242_isAnagram/main.py
@@ -1,5 +1,17 @@
 # This is an exclusive problem only available in the Complete and Deluxe versions of this course.
 # To learn more, visit https://kaeducation.com/lc-py.html
 
-class Solution:
+
+class Solution(object):
     def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        if len(s) != len(t):
+            return False
+        if sorted(s) == sorted(t):
+            return True
+        return False
