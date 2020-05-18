8b7ab5be077ecefa03b6e90883d5cfc93bde66fa
Aswin A V <aswinavofficial@gmail.com>
2020-05-19 01:13:02 +0530

    Solved LC49 Group Anagrams

--- strings/LC49_groupAnagrams/main.py
+++ strings/LC49_groupAnagrams/main.py
@@ -1,5 +1,23 @@
 # This is an exclusive problem only available in the Complete and Deluxe versions of this course.
 # To learn more, visit https://kaeducation.com/lc-py.html
 
-class Solution:
+
+class Solution(object):
     def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
+        d = {}
+        l = []
+        j = 0
+
+        for i in strs:
+            k = "".join(sorted(i))
+
+            if k in d.keys():
+                d[k].append(i)
+            else:
+                d[k] = [i]
+        return d.values()
