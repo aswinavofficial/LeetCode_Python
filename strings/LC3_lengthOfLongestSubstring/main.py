82628460b3de488b7d6519600f74e3e445171995
Aswin A V <aswinavofficial@gmail.com>
2020-05-18 23:23:06 +0530

    Solved LC3 - Length of Longest substring

--- strings/LC3_lengthOfLongestSubstring/main.py
+++ strings/LC3_lengthOfLongestSubstring/main.py
@@ -1,2 +1,21 @@
-class Solution:
+class Solution(object):
     def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        string = s
+        # Creating a set to store the last positions of occurrence
+        seen = {}
+        maximum_length = 0
+
+        # starting the inital point of window to index 0
+        start = 0
+
+        for end in range(len(string)):
+            # Checking if we have already seen the element or not
+            if string[end] in seen:
+                start = max(start, seen[string[end]] + 1)
+            seen[string[end]] = end
+            maximum_length = max(maximum_length, end - start + 1)
+        return maximum_length
