d63fdf7b8ff1b428d88fc5f33def96c898c26ba0
Aswin A V <aswinavofficial@gmail.com>
2020-05-20 00:12:52 +0530

    LC20 : Valid Parenthesis solved

--- strings/LC20_isValid/main.py
+++ strings/LC20_isValid/main.py
@@ -1,5 +1,22 @@
 # This is an exclusive problem only available in the Deluxe version of this course.
 # To learn more, visit https://kaeducation.com/lc-py.html
 
-class Solution:
+class Solution(object):
     def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        stack = []
+        dictp = {"(":")","{":"}","[":"]"}
+        for i in s:
+            
+            if i in dictp.keys():
+                stack.append(i)
+                
+            elif  len(stack) == 0 or dictp[stack.pop()] != i:
+                return False
+            
+        return len(stack) == 0
+        
