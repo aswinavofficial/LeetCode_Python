a57e26c54970dc5d5676f080749234e6676e4ae3
Aswin A V <aswinavofficial@gmail.com>
2020-05-21 00:42:46 +0530

    Solved LC844 BackSpaceCompare using stacks

--- practice/LC844_backspaceCompare/main.py
+++ practice/LC844_backspaceCompare/main.py
@@ -1,5 +1,32 @@
 # This is an exclusive problem only available in the Deluxe version of this course.
 # To learn more, visit https://kaeducation.com/lc-py.html
 
-class Solution:
+
+class Solution(object):
     def backspaceCompare(self, S, T):
+        """
+        :type S: str
+        :type T: str
+        :rtype: bool
+        """
+        stack1 = []
+        stack2 = []
+        for i in S:
+            if i != "#":
+                stack1.append(i)
+            elif len(stack1) > 0:
+                stack1.pop()
+
+        for i in T:
+            if i != "#":
+                stack2.append(i)
+            elif len(stack2) > 0:
+                stack2.pop()
+        if len(stack1) != len(stack2):
+            return False
+
+        for i in range(len(stack1)):
+            if stack1[i] != stack2[i]:
+                return False
+        return True
+
