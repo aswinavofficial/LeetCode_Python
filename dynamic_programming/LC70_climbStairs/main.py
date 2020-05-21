e3f8f490d668ca900edd0bd95b5cc3034b35eb50
Aswin A V <aswinavofficial@gmail.com>
2020-05-21 23:24:48 +0530

    Solved LC70 ClimbStairs problem

--- dynamic_programming/LC70_climbStairs/main.py
+++ dynamic_programming/LC70_climbStairs/main.py
@@ -1,2 +1,17 @@
-class Solution:
+class Solution(object):
     def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        if n <= 3:
+            return n
+
+        w = [0, 1, 2, 3]
+        i = 4
+        while i <= n:
+            print("wi-2", w[i - 2])
+            w.append(w[i - 1] + w[i - 2])
+            i = i + 1
+        return w[n]
